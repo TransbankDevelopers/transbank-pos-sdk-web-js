553c181d59e4b0b7e0f14e027ea577ad0cc5a96a
Matias Vasquez <m.vasquezac@gmail.com>
2023-10-11 11:37:26 -0300

    feat: update default url

--- src/pos.js
+++ src/pos.js
@@ -23,7 +23,7 @@ export class TransbankPOSWebSocket extends EventEmitter {
         return this.socket;
     }
 
-    async connect(socketIoUrl = "http://localhost:8090") {
+    async connect(socketIoUrl = "https://localhost:8090") {
         this.socket = io(socketIoUrl)
         this.isConnected = true
 
