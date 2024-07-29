7a14b0619e789eee1d65f63191808be91be29943
Matias Vasquez <m.vasquezac@gmail.com>
2024-07-29 16:17:09 -0400

    feat: add index to export responses

---
+++ types/responses/index.d.ts
@@ -0,0 +1,7 @@
+export * from './saleResponse';
+export * from './loadKeysResponse';
+export * from './totalsResponse';
+export * from './refundResponse';
+export * from './detailsResponse';
+export * from './closeResponse';
+export * from './portStatusResponse';
