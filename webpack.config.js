75b4a44fa2b73d2b41d5ac8efbb4a665d13d877d
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2024-08-07 11:35:32 -0400

    chore: remove babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -15,11 +15,6 @@ module.exports = [
     },
     module: {
         rules: [
-            {
-                test: /\.(js)$/,
-                exclude: /(node_modules|bower_components)/,
-                use: ['babel-loader']
-            }
         ]
     },
     devtool,
